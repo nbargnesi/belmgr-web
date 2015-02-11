8690c77b734ed5d3d94b72c9bfee256d13099939
Nick Bargnesi <nbargnesi@selventa.com>
2015-02-11 16:46:39 -0500

    change NAME/HELP of grunt build script

--- scripts/02-grunt-build.sh
+++ scripts/02-grunt-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # The next three lines are for the go shell.
-export SCRIPT_NAME="grunt serve"
-export SCRIPT_HELP="Serve the app via Grunt."
+export SCRIPT_NAME="grunt build"
+export SCRIPT_HELP="Build the app via Grunt."
 [[ "$GOGO_GOSH_SOURCE" -eq 1 ]] && return 0
 
 # Normal script execution starts here.
