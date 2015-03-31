e31b60ac656c8679fd37f0c8b49382c620bb4207
gshih501 <gshih@Anselmos-MacBook-Pro.local>
2015-03-31 12:29:44 -0400

    update search page

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,124 +1,126 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  /**
-   * @ngdoc function
-   * @name belmgrWebApp.controller:SearchController
-   * @description
-   * # SearchController
-   * Controller of the belmgrWebApp
-   */
+    /**
+     * @ngdoc function
+     * @name belmgrWebApp.controller:SearchController
+     * @description
+     * # SearchController
+     * Controller of the belmgrWebApp
+     */
 
-  angular.module('belmgrWebApp')
-      .controller('SearchController', searchController);
+    angular.module('belmgrWebApp')
+        .controller('SearchController', searchController);
 
-  searchController.$inject = ['$scope', '$filter', 'searchService'];
+    searchController.$inject = ['$scope', '$filter', 'searchService'];
 
-  function searchController($scope, $filter, searchService) {
+    function searchController($scope, $filter, searchService) {
 
-    $scope.init = function() {
-        $scope.searchQuery = '';
-        $scope.statusFacet = true;
-        // this is the default settings for evidence search
-        $scope.evidenceSetting = new evidenceSetting();
-        // runs the search with the default settings as the init results
-        searchService.getEvidenceCollection($scope.evidenceSetting, loadEvidence);
-    };
+            $scope.init = function() {
+                $scope.searchQuery = '';
+                $scope.statusFacet = true;
+                $scope.currentPosition = 0;
+                // this is the default settings for evidence search
+                $scope.evidenceSetting = new evidenceSetting();
+                // runs the search with the default settings as the init results
+                searchService.getEvidenceCollection($scope.evidenceSetting, loadEvidence);
+            };
 
-    // the evidence setting class
-    function evidenceSetting() {
-        this.start = 0;
-        this.size = 100;
-        this.facet = $scope.statusFacet;
-        this.filterOptions = createFilterOptions();
-    }
+            // the evidence setting class
+            function evidenceSetting() {
+                this.start = $scope.currentPosition;
+                this.size = 100;
+                this.facet = $scope.statusFacet;
+                this.filterOptions = createFilterOptions();
+            }
 
-    function createFilterOptions() {
-        var filterOptions = null;
-        $scope.additionalFilters = $scope.selectedFacets;
-        return filterOptions;
-    }
+            function createFilterOptions() {
+                var filterOptions = null;
+                $scope.additionalFilters = $scope.selectedFacets;
+                return filterOptions;
+            }
 
-    $scope.getEvidenceCollectionNext = function() {
-        var previous = $scope.evidenceSetting.start;
-        $scope.evidenceSetting = new evidenceSetting();
-        $scope.evidenceSetting.start = previous + 100;
-        searchService.getEvidenceCollection($scope.evidenceSetting, loadEvidence, $scope.additionalFilters);
-    };
+            $scope.getEvidenceCollectionNext = function() {
+                $scope.currentPosition = $scope.currentPosition + 100;
+                $scope.evidenceSetting = new evidenceSetting();
+                searchService.getEvidenceCollection($scope.evidenceSetting, updateResult, $scope.additionalFilters);
+            };
 
-    $scope.getEvidenceCollectionPrev = function() {
-        var previous = $scope.evidenceSetting.start;
-        $scope.evidenceSetting = new evidenceSetting();
-        if (previous >= 100) {
-            $scope.evidenceSetting.start = previous - 100;
-        } else {
-            $scope.evidenceSetting.start = 0
-        }
-        searchService.getEvidenceCollection($scope.evidenceSetting, loadEvidence, $scope.additionalFilters);
-    };
+            $scope.getEvidenceCollectionPrev = function() {
+                if ($scope.currentPosition >= 100) {
+                    $scope.currentPosition = $scope.currentPosition - 100;
+                } else {
+                    $scope.currentPosition = 0
+                }
+                $scope.evidenceSetting = new evidenceSetting();
+                searchService.getEvidenceCollection($scope.evidenceSetting, updateResult, $scope.additionalFilters);
+            };
 
-    function loadEvidence(collection, facets) {
-      $scope.evidenceCollection = collection;
-      $scope.evidenceCollection.forEach(function(evidence) {
-          evidence.show = true;
-      });
-      $scope.facetsSet = [];
+            function updateResult(collection, facets) {
+                $scope.evidenceCollection = collection;
+                $scope.$digest(['$scope.evidenceCollection', '$scope.facetsSet']);
+            }
 
-      // this is hard coded
-      var species = {
-          facetGroup: 'Species',
-          content: [],
-          filters: ''
-      };
-      var status = {
-          facetGroup: 'Status',
-          content: [],
-          filters: ''
-      };
+            function loadEvidence(collection, facets) {
+                $scope.evidenceCollection = collection;
+                $scope.facetsSet = [];
 
-      // show the facets
-      facets.forEach(function(facet) {
-          if (facet.filter.category === 'biological_context' && facet.filter.name === "Species") {
-              if (!angular.isArray(facet.filter.value)) {
-                  $scope.selectedFacets.forEach(function(item) {
-                      if (item.value === facet.filter.value) {
-                          facet.selected = true;
-                      }
-                  });
-                  species.content.push(facet);
-              }
-          } else if (facet.filter.category === 'metadata' && facet.filter.name === "status") {
-              $scope.selectedFacets.forEach(function(item) {
-                  if (item.value === facet.filter.value) {
-                      facet.selected = true;
-                  }
-              });
-              if (!angular.isArray(facet.filter.value)) {
-                  status.content.push(facet);
-              }
-          }
-      });
+                // this is hard coded
+                var species = {
+                    facetGroup: 'Species',
+                    content: [],
+                    filters: ''
+                };
+                var status = {
+                    facetGroup: 'Status',
+                    content: [],
+                    filters: ''
+                };
 
-      $scope.facetsSet.push(species, status);
-      $scope.$apply(['$scope.evidenceCollection', '$scope.facetsSet']);
-    }
+                // show the facets
+                facets.forEach(function(facet) {
+                    if (facet.filter.category === 'biological_context' && facet.filter.name === "Species") {
+                        if (!angular.isArray(facet.filter.value)) {
+                            species.content.push(facet);
+                        }
+                    } else if (facet.filter.category === 'metadata' && facet.filter.name === "status") {
 
-    $scope.selectedFacets = [];
+                        if (!angular.isArray(facet.filter.value)) {
+                            status.content.push(facet);
+                        }
+                    }
+                });
 
-    $scope.facetFilter = function(filterObject, applyFilter) {
-        var filter = belhop.factory.options.filter.custom(filterObject.category, filterObject.name, filterObject.value)
-        if (applyFilter === true) {
-            $scope.selectedFacets.push(filter);
-        } else {
-            $scope.selectedFacets.splice($scope.selectedFacets.indexOf(filter), 1);
-        }
-    };
+                $scope.facetsSet.push(species, status);
+                $scope.$digest(['$scope.evidenceCollection', '$scope.facetsSet']);
+            }
 
-    $scope.searchEvidence = function() {
-        // create new search options
-        $scope.evidenceSetting = new evidenceSetting();
-        searchService.getEvidenceCollection($scope.evidenceSetting, loadEvidence, $scope.additionalFilters);
-    };
+            $scope.selectedFacets = [];
+            $scope.displayDetail = [];
+            $scope.showDetail = function(id) {
+                if ($scope.displayDetail[id] === false || $scope.displayDetail[id] === undefined) {
+                    $scope.displayDetail[id] = true;
+                } else {
+                    $scope.displayDetail[id] = false;
+                }
 
-  } // end of SearchController
+            }
+            $scope.facetFilter = function(filterObject, applyFilter) {
+                var filter = belhop.factory.options.filter.custom(filterObject.category, filterObject.name, filterObject.value)
+                if (applyFilter === true) {
+                    $scope.selectedFacets.push(filter);
+                    $scope.searchEvidence();
+                } else {
+                    $scope.selectedFacets.splice($scope.selectedFacets.indexOf(filter), 1);
+                    $scope.searchEvidence();
+                }
+            };
+
+            $scope.searchEvidence = function() {
+                // create new search options
+                $scope.evidenceSetting = new evidenceSetting();
+                searchService.getEvidenceCollection($scope.evidenceSetting, updateResult, $scope.additionalFilters);
+            };
+
+        } // end of SearchController
 }());
--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -1,46 +1,47 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  /**
-   * @ngdoc service
-   * @name belmgrWebApp.search
-   * @description
-   * # search
-   * Service in the belmgrWebApp.
-   */
-  angular.module('belmgrWebApp')
-      .factory('searchService', searchService);
+    /**
+     * @ngdoc service
+     * @name belmgrWebApp.search
+     * @description
+     * # search
+     * Service in the belmgrWebApp.
+     */
+    angular.module('belmgrWebApp')
+        .factory('searchService', searchService);
 
-  function searchService() {
-    var service = {
-        getEvidenceCollection: getEvidenceCollection,
-    };
-    return service;
+    function searchService() {
+            var service = {
+                getEvidenceCollection: getEvidenceCollection,
+            };
+            return service;
 
-    function getEvidenceCollection(setting, cb, additionalFilters) {
+            function getEvidenceCollection(setting, cb, additionalFilters) {
 
-      function _success(responseData, statusString, request) {
-          cb(responseData.evidence, responseData.facets);
-      }
+                function _success(responseData, statusString, request) {
+                    console.log(responseData);
+                    cb(responseData.evidence, responseData.facets);
+                }
 
-      function _error(request, errorString, exception) {
+                function _error(request, errorString, exception) {
 
-      }
+                }
 
-      var _cb = {
-          success: _success,
-          error: _error
-      };
-      //var FilterOptions = new belhop.__.FilterOptions('metadata', 'status', 'Draft');
-      
-      var searchOptions = new belhop.__.SearchOptions(setting.start, setting.size, setting.facet, setting.filterOptions);
-      if (setting.facet === true) {
-          belhop.evidence.search(searchOptions, _cb, {
-              additionalFilters: additionalFilters
-          });
-      } else {
-          belhop.evidence.search(searchOptions, _cb);
-      }
-    }
-  } // end of searchService
+                var _cb = {
+                    success: _success,
+                    error: _error
+                };
+                //var FilterOptions = new belhop.__.FilterOptions('metadata', 'status', 'Draft');
+
+                var searchOptions = new belhop.__.SearchOptions(setting.start, setting.size, setting.facet, setting.filterOptions);
+                if (setting.facet === true) {
+                    belhop.evidence.search(searchOptions, _cb, {
+                        additionalFilters: additionalFilters
+                    });
+                } else {
+                    belhop.evidence.search(searchOptions, _cb);
+                }
+            }
+        } // end of searchService
 }());
